1d09f48b9f4fa26f5f70d03a51180ffdba787614
GobindChoudhary <chaudharygobind24@gmail.com>
2025-03-12 20:14:12 +0530

    input output

---
+++ inp_out.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+     
+    // printf(" %d + %d = %d ",a  , b , a+b);
+    // printf("sun of %d and %d is %d ",a  , b , a+b);
+    // printf("Value of a is %d and value of b is %d",a  , b);
+    // printf("a = %d & b = %d",a  , b);
+    // printf("a = %d and b = %d",a  , b);
+    // printf("Welcome\nStudent");
+
+    // 1
+    // printf("hello student");
+    // 2
+    // printf("Hello\nstudent");
+    // 3
+    // printf(" \"MySirG\" ");
+    // 4
+    // printf(" \"\\n\"");
+    // 5 
+    // printf(" \"\\r\"");
+    // 6
+    // printf(" \"Teacher\'s Day\"");
+
+    // 7
+
+        // int a,b;
+    // printf("enter the value of a \n");
+    // scanf("%d",&a);
+    // printf("enter the value of b \n");
+    // scanf("%d",&b);
+    // printf("sum of %d and %d is %d", a,b,a+b );
+
+    // 8
+
+    // int a; 
+    // printf("Enter number to get its square: ");
+    // scanf("%d",&a);
+    // printf("square of %d is %d4",a , a*a);
+
+    // 9
+
+    // int length,bredth;
+    // printf("Enter length and bredth in meter:");
+    // scanf("%d %d",&length,&bredth);
+    // printf("The area of Rectangle is %d m^2",length*bredth);
+
+    // 10
+
+    // int radius;
+    // printf("enter radius:");
+    // scanf("%d",&radius);
+    // printf("Area of the circle is %.2f having the radius %d ",3.14*radius*radius,radius);
+
+    // 11
+
+    // int a,b,c;
+    // float avg;
+    // printf("Enter three Number to get there average \n");
+    // scanf("%d %d %d",&a,&b,&c);
+    // avg = (a+b+c)/3.0 ;
+    // printf("The average is %.2f",avg);
+
+    // 12
+
+    // float radius;
+    // printf("enter the value of radius:");
+    // scanf("%f",&radius);
+    // printf("The circumfrence of circle is %.2f", 2*3.14*radius);
+
+    // 13
+
+    // float p,r,t;
+
+    // printf("Enter Principal:");
+    // scanf("%f",&p);
+    // printf("Enter Rate of Intrest:");
+    // scanf("%f",&r);
+    // printf("Enter Time:");
+    // scanf("%f",&t);
+    // printf("Simple Intrest is: %.2f",(p*r*t)/100);
+    
+
+    // 14
+
+    // float h,l,b;
+
+    // printf("Enter Height in meter:");
+    // scanf("%f",&h);
+    // printf("Enter Length in meter:");
+    // scanf("%f",&l);
+    // printf("Enter bredth in meter:");
+    // scanf("%f",&b);
+    // printf("Volume is: %.2f m^3", h*l*b);
+
+    // 15
+
+    // float cp,sp;
+
+    // printf("Enter cost price ");
+    // scanf("%f",&cp);
+    // printf("Enter selling price ");
+    // scanf("%f",&sp);
+    // printf("profit and loss is %.2f" ,((sp - cp)*25)/12);
+
+    //16
+
+    // char c;
+    // printf("Enter a character ");
+    // scanf("%c",&c);
+    // printf("%d",c);
+
+    // 17
+    // int num;
+    // printf("Enter a character ");
+    // scanf("%d",&num);
+    // printf("%c",num);
+
+    // 18
+
+    // char a,b,c;
+    // printf("Enter three character ");
+    // scanf("%c%c%c",&a,&b,&c);
+    // printf(" %c - %d \n %c - %d \n %c - %d",a,a,b,b,c,c);
+
+    // 19
+    // int day,month,year;
+    // printf("enter day in \"DD/MM/YYYY\" formate: ");
+    // scanf("%d/%d/%d",&day,&month,&year);
+    // printf("Day - %d, Month - %d, Year - %d",day,month,year);
+    
+    // 20
+
+    // int hour,min;
+    // printf("enter time in \"HH:MM\" formate: ");
+    // scanf("%d:%d",&hour,&min);
+    // printf(" %d Hour and %d minute",hour,min);
+
+    return 0;
+}
