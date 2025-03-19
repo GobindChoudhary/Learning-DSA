50c623ebe1df94dad8a0c03cb8bf318813030d9b
GobindChoudhary <chaudharygobind24@gmail.com>
2025-03-19 17:18:41 +0530

    Assignment 4 completed

---
+++ Assign_4_operators.c
@@ -0,0 +1,80 @@
+
+#include <stdio.h>
+int main(){
+// 1. Write a program to input a three digit numbeil and display the sum of the digits.
+        // int num,sum = 0;
+        // scanf("%d",&num);
+        // while(num>0){
+        // sum += num%10;
+        // num = num /10;
+        // }
+        // printf("%d",sum);
+
+
+// 2. Write a program to find ASCII code of the character '+.
+        // char a = '+';
+        // printf("%d",a);
+
+
+// 3. Write a program to print size of an int, a float, a char and a double type variable
+        // int a ;
+        // char ch;
+        // float fl;
+        // double d;
+        // printf("sizeof int is %d \n",sizeof(a));
+        // printf("sizeof char is %d \n",sizeof(ch));
+        // printf("sizeof float is %d \n",sizeof(fl));
+        // printf("sizeof double is %d \n",sizeof(d));
+
+
+// 4. Write a program to make the last digit of a number stored in a variable as zero.
+// (Example - if x=2345 then make it x=2340)
+    //  int x;
+    //  scanf("%d",&x);
+
+    //  x = x/10;
+    //  x = x*10;
+
+    //  printf("%d",x);
+
+// 5. Write a program to input a number from the user and also input a digit. Append a
+// digit in the number and print the resulting number. (Example - number=234 and
+// digit=9 then the resulting number is 2349)
+
+    //  int x,digit,copy;
+    //  printf("Enter a number: \n");
+    //  scanf("%d",&x);
+    //  copy = x;
+    //  printf("Enter a digit: \n");
+    //  scanf("%d",&digit);
+    //  x = x * 10 + digit;
+    //  printf("The digit %d appended in number %d and the answer is %d",digit, copy, x);
+
+// Assume price of I USD is INR 84.23. Write a program to take the amount in INR
+// and convert it into USD.
+    // double rs , usd;
+    // printf("Enter the amount in rs. :");
+    // scanf("%lf",&rs);
+    // usd = rs/84.23;
+    // printf("Amount in USD become %.4lf USD",usd);
+// Write a program to take a three digit number from the user and rotate its digits by
+// one position towards the right. ex: 123  => 312
+    //   int x; 
+    //   printf("Enter 3 digit value:");
+    //   scanf("%d",&x);
+    //   printf("the answer is %d", x%10 * 100 + x/10 );
+     
+// What will be the value stored in the variable x after executing following statement:
+    // x = 10>8>4 // 0
+
+// What will be the value stored in the variable x after executing following statement:
+    // x = !2 > -2; //1
+    
+// 10. What will be the value stored in the variable x after executing following statement:
+    // x = 3 <0 && 5>0; //0
+    
+    return 0;
+} 
+      
+
+
