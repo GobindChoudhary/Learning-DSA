82d95a899c0d3b425b2bbd9623971860fa2bb103
GobindChoudhary <chaudharygobind24@gmail.com>
2025-03-19 17:18:21 +0530

    Assignment 3 completed

---
+++ Assign_3_operators.c
@@ -0,0 +1,98 @@
+ #include <stdio.h>
+
+ int main(){
+// 1.  Write a program to calculate size of a character constant.
+
+     // int a;
+     // a = sizeof('A');
+     // printf("%d",a);
+
+// 2.  Write a program to calculate size of a real constant.
+   
+     // int x;
+     // x = sizeof(2.2222);
+     // printf("%d",x);
+
+
+// 3.  Write a program with one char type variable. Assign 'A' in the variable. Now change
+//  the value of variable from 'A' to 'B' using increment operator.
+      
+     // char ch = 'A';
+     // ch++;
+     // printf("%d",ch);
+
+
+// 4.  Write a program to print unit digit of a given number
+      
+     // int a,x;
+     // a = 244;
+     // x = a % 10;
+     // printf("%d",x);
+
+// 5.  Write a program to print a given number without its last digit.
+
+     // int a,x;
+     // a = 244;
+     // x = a/10;
+     // printf("%d",x);
+
+
+// 6.  Write a program to swap values of two int variables
+     // int a,b,temp;
+     // a = 10;
+     // b = 20;
+
+     //    temp = a;
+     //    a = b;
+     //    b = temp;
+        
+     // printf("%d %d ",a,b);
+
+
+// 7.  Write a program to swap values of two int variables without using a third variable.
+     // int a,b;
+     // scanf("%d",&a);
+     // scanf("%d",&b);
+
+
+     // a = a + b;
+     // b = a - b;
+     // a = a - b;
+     
+     // printf("%d %d ",a,b);
+
+
+// 8.  Write a program to swap values of two int variables without using third variable and
+//  without using +, - operators.
+     // int a,b;
+     // scanf("%d",&a);
+     // scanf("%d",&b);
+     //  a = a*b;
+     //  b = a/b;
+     //  a = a/b;
+     // printf("%d %d ",a,b);
+
+
+// 9.  Write a program to swap values of two int variables without using third variable and
+//  arithmetic operators.
+
+     //   int a,b ;
+     //   scanf("%d",&a);
+     //   scanf("%d",&b);
+     //   a = a^b;
+     //   b = b^a;
+     //   a = b^a;
+     
+     //   printf("%d,%d",a,b);
+
+
+// 10. Write a program to swap values of two int variables in single line arithmetic
+//  expression.
+     // int a,b ;
+     // scanf("%d",&a);
+     // scanf("%d",&b);
+     // a = a+b - (b=a);
+     // printf("%d,%d",a,b);
+
+    return 0;
+ }
