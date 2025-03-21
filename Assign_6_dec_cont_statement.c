f816e70a01a40ccce20e03692443d0ee5d2a9064
GobindChoudhary <chaudharygobind24@gmail.com>
2025-03-21 22:30:59 +0530

    Assignment 6 on decision control statement completed

---
+++ Assign_6_dec_cont_statement.c
@@ -0,0 +1,165 @@
+#include <stdio.h>
+
+int main(){
+
+// 1.
+// Write a program to check whether a given number is a three digit number or not.
+
+        // int num ,count = 0, copy ;
+        
+        // scanf("%d",&num);
+        // copy = num;
+        // while (num>0)
+        // {
+        //     count++;
+        //     num = num /10;
+        // };
+        // if(count == 3 ){
+        //     printf("%d is 3 digit number",copy);
+        // }else{
+        //     printf("%d is not a 3 digit number",copy);
+        // }
+    
+
+
+// 2.
+// Write a program which takes the cost price and selling price of a product from the user. Now calculate and print profit or loss percentage.
+
+            // float cp,sp,incomePer;
+            // scanf("%f",&cp);
+            // scanf("%f",&sp);
+            // incomePer = ((sp -cp)/cp)*100.0;
+            // if(incomePer>0){
+            //    printf("The profit percentage is %.2f",incomePer);
+            // }else{
+            //     incomePer = incomePer * -1;
+            //    printf("The loss percentage is %.2f",incomePer);
+            // }
+
+// 3.
+// Write a program to take marks of 5 subjects from the user. Assume marks are given out of 100 and passing marks is 33. Now display whether the candidate passed the examination or failed.
+            
+        //    for (int i = 0; i < 5; i++)
+        //     {   int marks;
+        //         printf("Enter your Marks:\n");
+        //         scanf("%d",&marks);
+        //         if(marks > 33){
+        //             printf("You are pass \n");
+        //         }else{
+        //             printf("you failed the exam \n");
+        //             break;
+        //         }
+        //     }
+            
+
+// 4.
+// Write a program to check whether a given alphabet is in uppercase or lowercase.
+
+    //    char a ;
+    //    scanf("%d",&a);
+    //    if(a > 64 && a < 91){
+    //     printf("%c is Uppercase alphabet",a);
+    //    }else if (a > 96 && a < 123) {
+    //     printf("%c is lowercase alphabet",a);
+    //    }else{
+    //     printf("Enter a valid alphabet");
+    //    }
+        
+// 5.
+// Write a program to check whether a given number is divisible by 3 and divisible by 2.
+
+        //  int a;
+        //  scanf("%d",&a);
+        //  if (a%2 == 0 && a%3 == 0)
+        //  {
+        //     printf("%d is divisible by 3 and divisible by 2",a);
+        //  }else if(a%2 == 0){
+        //     printf("%d is divisible by 2 not by 3",a);
+        //  }else if(a%3 == 0){
+        //     printf("%d is divisible by 3 not by 2",a);
+        //  }else{
+        //     printf("%d is neither divisible by 3 nor by divisible 2",a);
+        //  }
+         
+// 6.
+// Write a program to check whether a given number is divisible by 7 or divisible by 3.
+
+        //  int a;
+        //  scanf("%d",&a);
+        //  if (a%7 == 0 && a%3 == 0)
+        //  {
+        //     printf("%d is divisible by 3 and divisible by 2",a);
+        //  }else if(a%7 == 0){
+        //     printf("%d is divisible by 7 not by 3",a);
+        //  }else if(a%3 == 0){
+        //     printf("%d is divisible by 3 not by 7",a);
+        //  }else{
+        //     printf("%d is neither divisible by 3 nor by divisible 7",a);
+         
+// 7.
+// Write a program to check whether a given number is positive, negative or zero.
+
+    // int a;
+    // scanf("%d",&a);
+    // if(a>0){
+    //     printf("%d is Positive",a);
+    // }else if (a == 0){
+    //     printf("%d is zero",a);
+    // }else{
+    //     printf("%d is Negative",a);
+    // };
+    
+// 8.
+// Write a program to check whether a given character is an alphabet (uppercase), an alphabet (lower case), a digit or a special character.
+// Write a program which takes the length of the sides of a triangle as an input.
+
+        // char a ;
+        // scanf("%d",&a);
+        // if(a > 64 && a < 91){
+        //     printf("%c is Uppercase alphabet",a);
+        // }else if (a > 96 && a < 123) {
+        //     printf("%c is lowercase alphabet",a);
+        // }else if (a>47 && a<58)
+        // {
+        //     printf("%c is a digit",a);
+        // }
+        // else{
+        //     printf("Enter a special character");
+        // }
+// 9.
+// Display whether the triangle is valid or not.
+
+        // int a,b,c;
+        // printf("Enter the angles of a trinagle: ");
+        // scanf("%d%d%d",a,b,c);
+        // if(a+b+c == 180){
+        // printf("The triangle is Valid");
+        // }else if {
+        // printf("The triangle is not Valid");
+        // }
+// 10. Write a program which takes the month number as an input and display number of
+// days in that month.
+
+        //    int month;
+        //    printf("Enter the month number: ");
+        //    scanf("%d",&month);
+        //    if(month<8){
+        //     if(month == 2){
+        //         printf("The number of days in this (%d) month is 28 and 29 in leap year",month);
+        //     }else if(month % 2 == 0){
+        //         printf("The number of days in this (%d) month is 30",month);
+        //     }else{
+        //         printf("The number of days in this (%d) month is 31",month);
+        //     }
+        //    }else{
+        //     if(month % 2 == 0){
+        //         printf("The number of days in this (%d) month is 31",month);
+        //     }else{
+        //         printf("The number of days in this (%d) month is 30",month);
+        //     }
+        //    }
+
+
+
+    return 0;
+}
