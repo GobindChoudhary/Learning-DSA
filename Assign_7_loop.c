d030d2e3dd405b7e2f8111b5d2d9ec738c54dd51
GobindChoudhary <chaudharygobind24@gmail.com>
2025-03-22 22:26:45 +0530

    Assignment 7 on loop completed

---
+++ Assign_7_loop.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+
+int main(){
+    // 1.Write a program to print MySirG 5 times on the screen
+            // for(int i = 1 ; i <=5; i++){
+            //     printf("MySirG \n");
+            // }
+
+
+    // 2.Write a program to print the first 10 natural numbers.
+            // for(int i = 1 ; i <=10; i++){
+            // printf("%d \n",i);
+            //  }
+    
+    // 3.Write a program to print the first 10 natural numbers in reverse order
+            // for(int i = 10 ; i >=1; i--){
+            //     printf("%d \n",i);
+            //     }
+    // 4.Write a program to print the first 10 odd natural numbers
+            //   for(int i = 1; i <=20;){
+            //     printf("%d \n",i);
+            //     i = i+2;
+            //   }
+    // 5.Write a program to print the first 1O odd natural numbers in reverse order.
+                // for(int i = 19; i >=1;){
+                //     printf("%d \n",i);
+                //     i = i-2;
+                // }
+    // 6.Write a program to print the first 10 even natural numbers
+                // int n = 0;
+                // while (n<20)
+                // {
+                //     if (n % 2 ==0)
+                //     {
+                //         printf("%d",n);
+                //     };
+                //     n++;
+                // }
+                
+    // 7.Write a program to print the first 10 even natural numbers in reverse order
+    
+    // int i = 1,n=10;
+    //     while(i <= n*2){
+    //         if(i%2 == 0){
+    //         printf("%d\n",i);
+    //         };
+    //         i++;
+    //     };
+
+    // 8.Write a program to print squares of the first 10 natural numbers
+    //  int n = 1;
+    //   while(n<10)
+    //   {
+    //      printf("%d\n",n*n);
+    //      n++;
+    //   };
+      
+    // 9.Write a program to print cubes of the first 10 natural numbers
+    // int n = 1;
+    //   while(n<10)
+    //   {
+    //      printf("%d\n",n*n*n);
+    //      n++;
+    //   };
+    // 10.Write a program to print a table of 5.
+    // int n = 1;
+    // int  num = 5;
+    // while(n<=10){
+    //     printf("%d * %d = %d\n",num,n,num*n);
+    //     n++;
+    // }
+
+    return 0 ;
+}
