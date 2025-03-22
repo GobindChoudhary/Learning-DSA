45ce656d6626895d764cb2546b2f1f1dc658359b
GobindChoudhary <chaudharygobind24@gmail.com>
2025-03-22 22:27:17 +0530

    Assignment 8 on loop completed

---
+++ Assign_8_loop.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+
+int main(){
+    // 1.Write a program to print MySirG N times on the screen
+            // int limit;
+            // scanf("%d",&limit);
+            // for(int i = 1 ; i <= limit; i++){
+            //     printf("MySirG \n");
+            // }
+
+
+    // 2.Write a program to print the first N natural numbers.
+            // int limit;
+            // scanf("%d",&limit);
+            // for(int i = 1 ; i <= limit; i++){
+            // printf("%d \n",i);
+            //  }
+    
+    // 3.Write a program to print the first N natural numbers in reverse order
+            // int limit;
+            // scanf("%d",&limit);
+            // for(int i = limit ; i >=1; i--){
+            //     printf("%d \n",i);
+            //     }
+    // 4.Write a program to print the first N odd natural numbers
+            // int limit;
+            // scanf("%d",&limit);
+            //   for(int i = 1; i <=limit;){
+            //     printf("%d \n",i);
+            //     i = i+2;
+            //   }
+    // 5.Write a program to print the first N odd natural numbers in reverse order.
+                // int limit;
+                // scanf("%d",&limit);
+                // for(int i = limit; i >=1;){
+                //     printf("%d \n",i);
+                //     i = i-2;
+                // }
+    // 6.Write a program to print the first N even natural numbers
+                // int n = 0;
+                // int limit;
+                // scanf("%d",&limit);
+                // while (n <= limit)
+                // {
+                //     if (n % 2 ==0)
+                //     {
+                //         printf("%d",n);
+                //     };
+                //     n++;
+                // }
+                
+    // 7.Write a program to print the first N even natural numbers in reverse order
+        int n ,i = 1;
+        scanf("%d",&n);
+        while(i <= n*2){
+            if(i%2 == 0){
+            printf("%d\n",i);
+            };
+            i++;
+        };
+      
+
+    // 8.Write a program to print squares of the first N natural numbers
+    //  int n = 1;
+    // int limit;
+    // scanf("%d",&limit);
+    //   while(n<=limit)
+    //   {
+    //      printf("%d\n",n*n);
+    //      n++;
+    //   };
+    
+    // 9.Write a program to print cubes of the first N natural numbers
+
+    // int n = 1;
+    // int limit;
+    // scanf("%d",&limit);
+
+    //   while(n<=limit)
+    //   {
+    //      printf("%d\n",n*n*n);
+    //      n++;
+    //   };
+
+    // 10.Write a program to print a table of N.
+    // int n = 1;
+    // int num;
+    // scanf("%d",&num);
+    // while(n<=10){
+    //     printf("%d * %d = %d\n",num,n,num*n);
+    //     n++;
+    // }
+
+return 0 ;
+    }
+    
